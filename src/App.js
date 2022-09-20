5ffad267eca7693ed194e63baebdaea3af5d3a2a
Edier Gama <ediersantiago7@gmail.com>
2022-09-19 19:32:29 -0500

    Initial commit, starting the project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import TodoList from './TodoList';
+import './App.css';
+
+function App() {
+
+    <TodoList />
+
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoList() {
+    <h1>
+        Hola react
+    </h1>
+}
+
+export default TodoList
